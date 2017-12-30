d188f5b2569a0956df203a6a867f61667ccc15d4
Taras Mandzak <tmandzak@gmail.com>
2017-12-30 14:07:04 +0100

    Simplest collision avoidance

--- src/main.cpp
+++ src/main.cpp
@@ -205,7 +205,7 @@ int main() {
   int lane = 1;
   
   //target reference velocity:
-  double ref_vel = 49.5;
+  double ref_vel = 0.0;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -245,6 +245,47 @@ int main() {
           	vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
           	
           	int prev_size = previous_path_x.size();
+          	
+          	
+          	if(prev_size > 2)
+          	{
+          	  car_s = end_path_s;
+          	}
+          	
+          	bool too_close = false;
+          	
+          	//find ref_y to use
+          	for (int i=0; i<sensor_fusion.size(); i++)
+          	{
+          	  //car is in my lane
+          	  float d = sensor_fusion[i][6];
+          	  if (d < (2 + 4*lane) && d > (2 + 4*lane - 2))
+          	  {
+          	    double vx = sensor_fusion[i][3];
+          	    double vy = sensor_fusion[i][4];
+          	    double check_speed = sqrt(vx*vx + vy*vy);
+          	    double check_car_s = sensor_fusion[i][5];
+          	    
+          	    // if using previous points can project s value out
+          	    check_car_s += (double)prev_size * 0.02 * check_speed;
+          	    
+          	    //check s values greater than mine and s gap
+          	    if((check_car_s > car_s) && ( check_car_s - car_s < 30))
+          	    {
+          	      too_close = true;
+          	    }
+          	  }
+          	  
+          	}
+          	
+          	if(too_close)
+          	{
+          	  ref_vel -= 0.224;
+          	}
+          	else if(ref_vel < 49.5)
+          	{
+          	  ref_vel += 0.224;
+          	}
           	
           	// Create a list of widely spaced (x, y) waypoints, evenly spaced at 30m
           	//Later we will interpolate these waypoints with a spline and fill it in with more points that control speed.
